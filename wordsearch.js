33473a6861f352e4f7e9f217bac9e44eb3a4fc5d
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-06-15 09:39:46 -0400

    Add the vertical check along with a test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
 const wordSearch = (letters, word) => { 
+    // check horizontal
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    // check vertical
+    let size = (horizontalJoin.length > 0) ? horizontalJoin[0].length : 0;
+    for (let i = 0; i < size; i++) {
+        const verticalJoin = horizontalJoin.map((horizontalWord) => {
+            return horizontalWord[i];
+        }).join("");
+        if (verticalJoin.includes(word)) return true;
+    }
+
+    // did not match anything
+    return false;
 }
 
 module.exports = wordSearch
